e0a456c76775b128184c53440e86eac895ede2c6
luantran96 <luantran96@yahoo.com>
2018-10-10 15:44:03 -0700

    fixed Pomander issues

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,17 @@
 var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chat'
+});
+
+module.exports(con);
+
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
 
+  
--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,33 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+
+CREATE TABLE rooms (
+  id int NOT NULL,
+  roomname VARCHAR(20),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id int NOT NULL,
+  username VARCHAR(20),
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int NOT NULL,
+  userID int,
+  roomID int,
+  contents VARCHAR(50),
+  PRIMARY KEY (id),
+  CONSTRAINT FK_userID FOREIGN KEY (userID) REFERENCES users(id),
+  CONSTRAINT FK_roomID FOREIGN KEY (roomID) REFERENCES rooms(id)
 );
+
 
 /* Create other tables and define schemas for them here! */
 
